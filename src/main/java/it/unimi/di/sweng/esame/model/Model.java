a1306b9bb107a3fbd158cd1ab9d7c7a3406dab8c
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 10:14:19 +0100

    VERDE: addRisolviSegnalazioneTest ora funziona

--- src/main/java/it/unimi/di/sweng/esame/model/Model.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 public class Model {
     private final @NotNull Map<Integer, Segnalazione> segnalazioni = new HashMap<>();
+    private final @NotNull Map<String, Segnalazione> risolte = new HashMap<>();
     public void addSegnalazione(@NotNull Segnalazione segnalazione) {
         if(segnalazioni.containsKey(segnalazione.km())){
             if(segnalazioni.get(segnalazione.km()).tratta().equals(segnalazione.tratta()))
@@ -21,6 +22,18 @@ public class Model {
         return new ArrayList<>(segnalazioni.values());
     }
 
-    public void removeSegnalazione(String s) {
+    public List<Segnalazione> getRisolte() {
+        return new ArrayList<>(risolte.values());
+    }
+
+    public void removeSegnalazione(@NotNull String segnalazione) {
+        String[] s = segnalazione.split(",");
+        int km = Integer.parseInt(s[1]);
+        if(segnalazioni.containsKey(km)){
+            if(segnalazioni.get(km).tratta().equals(s[0])){
+                risolte.put(segnalazioni.get(km).tratta(), segnalazioni.get(km));
+                segnalazioni.remove(km);
+            }
+        }
     }
 }
