3302edfafd3035c23e98de97cf584371ac495489
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 11:09:02 +0100

    REFACTORING

---
+++ src/main/java/it/unimi/di/sweng/esame/presenters/DisplayRisoltePresenter.java
@@ -0,0 +1,9 @@
+package it.unimi.di.sweng.esame.presenters;
+
+import it.unimi.di.sweng.esame.model.Model;
+import it.unimi.di.sweng.esame.views.DisplayView;
+
+public class DisplayRisoltePresenter {
+    public DisplayRisoltePresenter(Model model, DisplayView view, RisolteStrategy risolteStrategy) {
+    }
+}
---
+++ src/test/java/it/unimi/di/sweng/esame/presenters/DisplayRisoltePresenterTest.java
@@ -0,0 +1,26 @@
+package it.unimi.di.sweng.esame.presenters;
+
+import it.unimi.di.sweng.esame.model.Model;
+import it.unimi.di.sweng.esame.model.Segnalazione;
+import it.unimi.di.sweng.esame.views.DisplayView;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+class DisplayRisoltePresenterTest {
+    @Test
+    void testUpdateRisolte() {
+        Model model = spy(Model.class);
+        DisplayView view = mock(DisplayView.class);
+        DisplayRisoltePresenter SUT = new DisplayRisoltePresenter(model, view, new RisolteStrategy());
+        model.addSegnalazione(Segnalazione.creaSegnalazione("A4,54,Incidente"));
+        model.addSegnalazione(Segnalazione.creaSegnalazione("A1,97,Traffico"));
+        model.addSegnalazione(Segnalazione.creaSegnalazione("A5,97,Traffico"));
+        model.removeSegnalazione("A1,97");
+        model.removeSegnalazione("A4,54");
+        verify(view).set(0, "Traffico sulla A1 al km 97");
+        verify(view).set(1, "Traffico sulla A4 al km 54");
+    }
+}
