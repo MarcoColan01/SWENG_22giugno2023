ffff440d6cd081a2ce0ad52db1cdc9e31bf71974
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 10:07:15 +0100

    VERDE: testAddSegnalazioneDoppia ora funziona

--- src/main/java/it/unimi/di/sweng/esame/model/Model.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -10,6 +10,10 @@ import java.util.Map;
 public class Model {
     private final @NotNull Map<Integer, Segnalazione> segnalazioni = new HashMap<>();
     public void addSegnalazione(@NotNull Segnalazione segnalazione) {
+        if(segnalazioni.containsKey(segnalazione.km())){
+            if(segnalazioni.get(segnalazione.km()).tratta().equals(segnalazione.tratta()))
+                throw new IllegalArgumentException("altra segnalazione già presente per questo tratto");
+        }
         segnalazioni.put(segnalazione.km(), segnalazione);
     }
 
