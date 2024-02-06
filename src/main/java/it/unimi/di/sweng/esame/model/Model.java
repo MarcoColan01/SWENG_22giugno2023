4c7c19f1d46abfd9f4d88ab26ddf402dfc319706
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 10:01:33 +0100

    VERDE: addSegnalazioneTest ora funziona

--- src/main/java/it/unimi/di/sweng/esame/model/Model.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Model.java
@@ -1,12 +1,19 @@
 package it.unimi.di.sweng.esame.model;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Model {
-    public void addSegnalazione(Segnalazione segnalazione) {
+    private final @NotNull Map<Integer, Segnalazione> segnalazioni = new HashMap<>();
+    public void addSegnalazione(@NotNull Segnalazione segnalazione) {
+        segnalazioni.put(segnalazione.km(), segnalazione);
     }
 
     public List<Segnalazione> getSegnalazioni() {
-        return null;
+        return new ArrayList<>(segnalazioni.values());
     }
 }
