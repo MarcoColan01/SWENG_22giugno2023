6d6a0708f2981e1be4a13b6002ec63faaf18c5a0
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 09:39:14 +0100

    VERDE: testTrattoStradaSbagliato ora funziona

--- src/main/java/it/unimi/di/sweng/esame/presenters/CentralStationPresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenters/CentralStationPresenter.java
@@ -1,13 +1,21 @@
 package it.unimi.di.sweng.esame.presenters;
 
 import it.unimi.di.sweng.esame.views.CentralStationView;
+import org.jetbrains.annotations.NotNull;
 
 public class CentralStationPresenter implements Presenter{
-    public CentralStationPresenter(CentralStationView view) {
+    private final @NotNull CentralStationView view;
+    public CentralStationPresenter(@NotNull CentralStationView view) {
+        this.view = view;
+        view.addHandlers(this);
     }
 
     @Override
-    public void action(String text1, String text2) {
-        
+    public void action(String comando, String args) {
+        String[] arg = args.split(",");
+        if(!arg[0].matches("[A-Z]\\d")){
+            view.showError("campo tratto non valido");
+            return;
+        }
     }
 }
