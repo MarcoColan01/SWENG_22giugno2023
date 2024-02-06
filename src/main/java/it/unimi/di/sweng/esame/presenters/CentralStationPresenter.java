17c7503eec461b1dd78c5a00380864fe3af3e350
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 09:36:54 +0100

    ROSSO: testTrattoStradaSbagliato fallisce

---
+++ src/main/java/it/unimi/di/sweng/esame/presenters/CentralStationPresenter.java
@@ -0,0 +1,13 @@
+package it.unimi.di.sweng.esame.presenters;
+
+import it.unimi.di.sweng.esame.views.CentralStationView;
+
+public class CentralStationPresenter implements Presenter{
+    public CentralStationPresenter(CentralStationView view) {
+    }
+
+    @Override
+    public void action(String text1, String text2) {
+        
+    }
+}
---
+++ src/test/java/it/unimi/di/sweng/esame/presenters/CentralStationPresenterTest.java
@@ -0,0 +1,18 @@
+package it.unimi.di.sweng.esame.presenters;
+
+import it.unimi.di.sweng.esame.views.CentralStationView;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class CentralStationPresenterTest {
+    @Test
+    void testTrattoStradaSbagliato(){
+        CentralStationView view = mock(CentralStationView.class);
+        CentralStationPresenter SUT = new CentralStationPresenter(view);
+        SUT.action("Segnala", "23,0,incidente");
+        verify(view).showError("campo tratto non valido");
+    }
+}
