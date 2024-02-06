df63fa366d70230feb9fbe92dbde805543b3fbf8
cola0 <marco.colangelo@studenti.unimi.it>
2024-02-06 10:05:51 +0100

    ROSSO. testAddSegnalazioneDoppia fallisce

--- src/test/java/it/unimi/di/sweng/esame/model/ModelTest.java
+++ src/test/java/it/unimi/di/sweng/esame/model/ModelTest.java
@@ -3,6 +3,7 @@ package it.unimi.di.sweng.esame.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
@@ -12,5 +13,13 @@ class ModelTest {
         Model SUT = new Model();
         SUT.addSegnalazione(Segnalazione.creaSegnalazione("A4,57,Incidente"));
         assertThat(SUT.getSegnalazioni()).hasSize(1);
+    }
+    @Test
+    void testAddSegnalazioneDoppia(){
+        Model SUT = new Model();
+        SUT.addSegnalazione(Segnalazione.creaSegnalazione("A4,57,Incidente"));
+        assertThatThrownBy(() -> SUT.addSegnalazione(Segnalazione.creaSegnalazione("A4,57,Incidente")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("altra segnalazione già presente per questo tratto");
     }
 }
